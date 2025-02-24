b81505837eb9d933fe9d974be468cacfabb5b9a2
cufta22 <cufta22@proton.me>
2025-02-24 23:30:59 +0100

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { exec } from "node:child_process";
+// import crypto from "node:crypto";
 
 // Load the environment variables
 import * as dotenv from "dotenv";
@@ -23,13 +24,23 @@ app.post("/postreceive", (req, res) => {
 	const payload = req.body;
 
 	// Optional: Verify the GitHub webhook signature (using the secret key)
-	// Add your logic here to verify the webhook's authenticity
+	// const secret = process.env.GITHUB_WEBHOOK_SECRET; // Store your secret in .env
+	// const signature = req.headers.get("X-Hub-Signature");
+	// const hash = crypto.createHmac("sha1", secret).update(payload).digest("hex");
 
-	console.log("Received webhook: ");
-	console.log(payload);
+	// if (`sha1=${hash}` !== signature) {
+	// 	console.error("Invalid signature");
+	// 	return new Response("Invalid signature", { status: 400 });
+	// }
+
+	// Extract the branch name from the "ref" field
+	const ref = payload.ref; // e.g., "refs/heads/main"
+	const branch = ref.split("/")[2]; // This will give "main" (or any branch name)
+
+	const scriptPath = branch === "master" ? PATHS.production : PATHS.staging;
 
 	// Trigger deployment script (e.g., pulling the latest changes)
-	exec(`bash ${PATHS.staging}`, (err, stdout, stderr) => {
+	exec(`bash ${scriptPath}`, (err, stdout, stderr) => {
 		if (err) {
 			return res.status(500).send("Error during redeployment");
 		}
